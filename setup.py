bf2b8a8b9cba08388b9a7a9f1ee000154b7f8088
maany <imptodefeat@gmail.com>
2018-01-26 16:18:43 +0100

    remove ci configurations for pypy

--- setup.py
+++ setup.py
@@ -81,11 +81,11 @@ setup(
     # },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license='Apache Software License',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -94,8 +94,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        #'Programming Language :: Python :: Implementation :: CPython',
+        #'Programming Language :: Python :: Implementation :: PyPy'
     ],
     # $ setup.py publish support.
     cmdclass={
