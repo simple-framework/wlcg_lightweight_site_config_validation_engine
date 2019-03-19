4f47c95de9fa42b2ecc26f946eb3afab87817bd4
Micky <yun_chan4@hotmail.com>
2019-03-19 20:59:21 +0200

    add schema and test for site object and more

--- config_validation_engine/validators/tests/test_validate.py
+++ config_validation_engine/validators/tests/test_validate.py
@@ -1,11 +1,29 @@
 from config_validation_engine.validators import validators as val
+import unittest
+import yamale
 
-def test_email():
-    v = val.Email()
-    assert not v.is_valid('random@')
 
-def test_latitude():
-    v = val.Latitude()
-    assert v.is_valid(23)
-    assert not v.is_valid(23.1)
-    
+class MyTest(unittest.TestCase):
+
+    def test_email(self):
+        v = val.Email(self)
+        assert not v.is_valid('random@')
+
+    def test_latitude(self):
+        v = val.Latitude()
+        assert v.is_valid(23)
+        assert v.is_valid(23.1)
+
+    def test_URL(self):
+        v = val.URL()
+        assert not v._is_valid('test.com')
+        assert v._is_valid('http://test.com')
+
+    def test_site_object(self):
+        schema = yamale.make_schema('./schema/site_object_schema.yaml', validators=val.all_config_validators())
+        data = yamale.make_data('./tests/site_object_data.yaml')
+        assert yamale.validate(schema, data)
+
+
+if __name__ == '__main__':
+    unittest.main()
