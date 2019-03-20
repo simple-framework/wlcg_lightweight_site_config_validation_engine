1512b9992a59820a221057e92a15007437685e93
Micky <yun_chan4@hotmail.com>
2019-03-20 22:09:44 +0200

    add schema and test for site object and more

--- config_validation_engine/validators/validators.py
+++ config_validation_engine/validators/validators.py
@@ -1,18 +1,23 @@
-import yamale
 from yamale.validators import DefaultValidators, Validator
 from email.utils import parseaddr
+from urlparse import urlparse
+from math import ceil, floor
+
 
 class Email(Validator):
     """Email Validator"""
     tag = 'email'
-    def _is_valid(self,value):
+
+    def _is_valid(self, value):
         parseaddr(value)
+
 
 class Latitude(Validator):
     """Latitude Validator"""
     tag = 'latitude'
-    def _is_valid(self,value):
-        if(value in range(-90,90)):
+
+    def _is_valid(self, value):
+        if(all(i in range(-90, 90) for i in [ceil(value), floor(value)])):
             return True
         else:
             return False
@@ -20,11 +25,13 @@ class Latitude(Validator):
     def fail(self, value):
         return '\'%s\' is not a valid %s value. The acceptable range of values is %s' % (value, self.tag, '-90 to 90')
 
+
 class Longitude(Validator):
     """Longitude Validator"""
     tag = 'longitude'
-    def _is_valid(self,value):
-        if(value in range(-180,180)):
+
+    def _is_valid(self, value):
+        if(all(i in range(-180, 180) for i in [ceil(value), floor(value)])):
             return True
         else:
             return False
@@ -32,11 +39,17 @@ class Longitude(Validator):
     def fail(self, value):
         return '\'%s\' is not a valid %s value. The acceptable range of values is %s' % (value, self.tag, '-180 to 180')
 
+
 class URL(Validator):
     """URL Validator"""
     tag = 'url'
-    def _is_valid(self,value):
-        return false
+
+    def _is_valid(self, value):
+        url = urlparse(value)
+        if all([url.scheme, url.netloc]):
+            return True
+        return False
+
 
 def all_config_validators():
     validators = DefaultValidators.copy()
@@ -45,4 +58,3 @@ def all_config_validators():
     validators[Latitude.tag] = Latitude
     validators[URL.tag] = URL
     return validators
-
